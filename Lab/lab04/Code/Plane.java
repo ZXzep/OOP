d85cdd012f329021c6a18678624ab2bdc923ec9a
Xzep <zephyrxzep@gmail.com>
2022-08-31 00:42:45 +0700

    move files

---
+++ Lab/lab04/Code/Plane.java
@@ -0,0 +1,18 @@
+public class Plane extends Vehicle{
+    public void showPlaneInfo(){
+        System.out.println("Plane detail is, Fuel is "+this.getFuel()+" litre and Top Speed is "+this.getTopSpeed()+" m/s");
+    }
+    public void setPlaneInfo(int s, String t){
+        this.setFuel(s);
+        this.setTopSpeed(t);
+    }
+    public void fly(){
+        if (this.getFuel() >= 200){
+            System.out.println("Fly.");
+            this.setFuel(-200);
+        }
+        else{
+            System.out.println("Please add fuel.");
+        }
+    }
+}
