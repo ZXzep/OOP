b70e9c2b12968f32253af0ce0753536060987040
Xzep <zephyrxzep@gmail.com>
2022-11-02 14:08:11 +0700

    edits

--- lab11/Student.java
+++ lab11/Student.java
@@ -2,20 +2,23 @@ package lab11;
 
 import java.io.Serializable;
 
-public class Student implements Serializable{
+public class Student implements Serializable {
     private String name;
     private int ID;
     private int money;
-    public Student(){
+
+    public Student() {
         name = "";
         ID = 0;
         money = 0;
     }
-    public Student(String name, int ID, int money){
-        this.name = "";
-        this.ID = 0;
-        this.money = 0;
+
+    public Student(String name, int ID, int money) {
+        this.name = name;
+        this.ID = ID;
+        this.money = money;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -25,11 +28,10 @@ public class Student implements Serializable{
     }
 
     public void setMoney(int money) {
-        if(money < 0){
+        if (money < 0) {
             this.money = 0;
-        }
-        else{
-            this.money = money; 
+        } else {
+            this.money = money;
         }
     }
 
