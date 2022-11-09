18c41436948943a810ed45b6771009045c37e409
Xzep <zephyrxzep@gmail.com>
2022-11-09 10:07:19 +0700

    edits

---
+++ lab12/Poring.java
@@ -0,0 +1,14 @@
+package lab12;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+public class Poring extends JFrame implements Runnable, MouseListener{
+    private JFrame f;
+    private JPanel p;
+    private JTextField tf;
+    private JLabel l;
+    public Poring(int numberOfPoring){
+        
+    }
+}
